41ba971fe384f737c55a3452c8bbb8522202a59b
edoceo <code@edoceo.com>
2021-09-14 05:22:00 +0000

    stub in some key-pair create/save

--- boot.php
+++ boot.php
@@ -6,7 +6,55 @@
 define('APP_ROOT', __DIR__);
 require_once(__DIR__ . '/vendor/autoload.php');
 
-// ksort($_SERVER);
+error_reporting(E_ALL & ~E_NOTICE);
 
-// __exit_text($_SERVER);
-// print_r($_SERVER);exit;
+function _dbc($host)
+{
+	$sql_file = sprintf('%s/var/%s/database.sqlite', APP_ROOT, $host);
+	$sql_good = is_file($sql_file);
+
+	$dbc = new \Edoceo\Radix\DB\SQL(sprintf('sqlite:%s', $sql_file));
+	if ( ! $sql_good) {
+		$dbc->query('CREATE TABLE _saltfan (key PRIMARY KEY, val)');
+		$dbc->query('CREATE TABLE post_incoming (id PRIMARY KEY, link, name, body, meta)');
+		$dbc->query('CREATE TABLE post_outgoing (id PRIMARY KEY, link, name, body, meta)');
+	}
+
+	return $dbc;
+
+}
+
+
+/**
+ *
+ */
+function _exit_html($body, $name='Saltfan')
+{
+	ob_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="initial-scale=1, user-scalable=yes">
+<meta name="application-name" content="Saltfan">
+<meta name="theme-color" content="#0317a7">
+<link rel="stylesheet" href="https://nobscss.com/nobs.css" crossorigin="anonymous">
+<title><?= $name ?></title>
+</head>
+<body>
+	<?= $body ?>
+</body>
+</html>
+<?php
+	$html = ob_get_clean();
+	__exit_html($html);
+}
+
+
+function _text_to_html($t)
+{
+	// $pd = new ParseDownExtra();
+	// return $pd->text($t);
+	return $t;
+}
--- webroot/main.php
+++ webroot/main.php
@@ -9,9 +9,63 @@ header('cache-control: no-store,max-age=0');
 header('content-type: text/plain');
 
 
-print_r(get_included_files());
+// Host
+$host = $_SERVER['SERVER_NAME'];
+$host_path = sprintf('%s/var/%s', APP_ROOT, rawurlencode($host));
+if ( ! is_dir($host_path)) {
+	_exit_html('<h1>This Host is NOT configured</h1>');
+	// mkdir($host_path, 0775, true);
+}
 
+$dbc = _dbc($host);
+$chk = $dbc->fetchOne('SELECT val FROM _saltfan WHERE key = ?', [ 'site-ed25519-key-pair' ] );
+if (empty($chk)) {
+	$val = sodium_crypto_box_keypair();
+	$val = sodium_bin2base64($val, SODIUM_BASE64_VARIANT_URLSAFE_NO_PADDING);
+	$dbc->query('INSERT INTO _saltfan VALUES (:k, :v)', [
+		':k' => 'site-ed25519-key-pair',
+		':v' => $val,
+	]);
+	_exit_html("<h1>You had no SITE level keypair, they were created</h1><p>Copy this value and do NOT lose it</p><pre>$val</pre>");
+}
 
-print_r(memory_get_peak_usage(true));
+$chk = $dbc->fetchOne('SELECT val FROM _saltfan WHERE key = ?', [ 'user-ed25519-key-pair' ] );
+if (empty($chk)) {
+	$val = sodium_crypto_box_keypair();
+	$val = sodium_bin2base64($val, SODIUM_BASE64_VARIANT_URLSAFE_NO_PADDING);
+	$dbc->query('INSERT INTO _saltfan VALUES (:k, :v)', [
+		':k' => 'user-ed25519-key-pair',
+		':v' => $val,
+	]);
+	_exit_html("<h1>You had no USER level keypair, they were created</h1><p>Copy this value and do NOT lose it</p><pre>$val</pre>");
+}
+
+exit;
+
+// Path
+$path0 = $_SERVER['REQUEST_URI'];
+$path0 = strtok($path0, '?');
+// stanatize double slashed to remove them all
+$path1 = $path0;
+// $path1 = ltrim($path0, './');
+$path1 = preg_replace('/\/+/', '/', $path1);
+// if ($path0 != $path1) {
+// 	_exit_html("<h1>Should Redirect '$path0' =&gt; '$path1'</h1>");
+// }
+$path_list = explode('/', $path1);
+
+// Switch base from ActivityPub Object Type?
+switch ($path1) {
+case '':
+case '/': // home
+
+	$text = file_get_contents(sprintf('%s/home.md', $host_path));
+	$html = _text_to_html($text);
+	// $feed = $dbc->fetchAll('SELECT * FROM ')
+	_exit_html($html);
+}
+
+// print_r(get_included_files());
+// print_r(memory_get_peak_usage(true));
 
 exit(0);
