e46bd3f8e11b7fbc2b6730cce538d8ebdb700b3f
edoceo <code@edoceo.com>
2021-09-21 03:22:18 +0000

    cleanup some output code

--- lib/Output/JSON.php
+++ lib/Output/JSON.php
@@ -1,11 +1,22 @@
 <?php
 /**
- *
+ * JSON Type Output
  */
 
 namespace App\Output;
 
 class JSON
 {
+	private $body;
+
+	function __construct($body)
+	{
+		$this->body = $body;
+	}
+
+	function render()
+	{
+		__exit_json($this->body);
+	}
 
 }
