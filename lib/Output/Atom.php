bf989f44f643cf741d1c360edbd62947f1deea29
edoceo <code@edoceo.com>
2021-09-21 02:24:22 +0000

    Output Formatters

---
+++ lib/Output/Atom.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *
+ */
+
+namespace App\Output;
+
+class Atom
+{
+
+}
---
+++ lib/Output/HTML.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ */
+
+namespace App\Output;
+
+class HTML
+{
+	private $body;
+	private $name;
+
+	function __construct($body)
+	{
+		$this->body = $body;
+	}
+
+	function render()
+	{
+		ob_start();
+		require_once(__DIR__ . '/HTML-template.php');
+		return ob_get_clean();
+	}
+
+	function setPageTitle($x)
+	{
+		$this->name = $x;
+	}
+}
