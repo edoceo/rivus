421650477f9d68946feceb4f2f97ab7a218199f6
edoceo <code@edoceo.com>
2023-07-08 16:13:02 +0000

    forgot one

--- lib/Output/HTML.php
+++ lib/Output/HTML.php
@@ -10,18 +10,30 @@ class HTML
 	private $body;
 	private $name;
 
+	/**
+	 *
+	 */
 	function __construct($body)
 	{
 		$this->body = $body;
 	}
 
+	/**
+	 *
+	 */
 	function render()
 	{
+
+		header('cache-control: no-store,max-age=0');
+
 		ob_start();
-		require_once(__DIR__ . '/HTML-template.php');
+		require_once(__DIR__ . '/HTML/Body.php');
 		return ob_get_clean();
 	}
 
+	/**
+	 *
+	 */
 	function setPageTitle($x)
 	{
 		$this->name = $x;
