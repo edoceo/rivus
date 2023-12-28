fa34d219fa4453847af90092ba398bff0315a8d7
edoceo <code@edoceo.com>
2023-12-28 05:55:21 +0000

    stub make

---
+++ make.sh
@@ -0,0 +1,13 @@
+
+
+composer update -a
+
+npm install
+
+
+cp node_modules/bootstrap/dist/css/bootstrap.min.css ./webroot/vendor/bootstrap/
+cp node_modules/bootstrap/dist/css/bootstrap.min.css.map ./webroot/vendor/bootstrap/
+
+
+cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js ./webroot/vendor/bootstrap/
+cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map ./webroot/vendor/bootstrap/
